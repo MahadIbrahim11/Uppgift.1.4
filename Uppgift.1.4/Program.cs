d164e51accfd56fd6a7ab7d55de8de6de091db99
maib07001 <maib07001@E-PW05C37V>
2024-11-14 15:47:52 +0100

    Add project files.

---
+++ Uppgift.1.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Uppgift_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder < 16)
+            {
+                Console.WriteLine("du får inte vara med");
+            }
+             else if (ålder >19)
+            {
+                Console.WriteLine("du får inte vara med tävlingen");
+            }
+            else
+            {
+                Console.WriteLine("du får inte vara med");  
+            }
+
+        }
+    }
+}
